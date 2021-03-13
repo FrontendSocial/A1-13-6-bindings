b56ec4052073b49cf84cbe9d224ba184c6417ec3
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-03-13 21:26:43 +0800

    Bindings implemented for Product LIst and Product details page. Features covered 1. One way binding 2. Property binding 3. Event binding.

--- src/app/product-list/product-card/product-card.component.ts
+++ src/app/product-list/product-card/product-card.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
+  product = {
+    "id":3,
+    "name":"iPhone 11 pro",
+    "imageUrl":"/assets/images/iphone.jpg",
+    "rating":5,
+    "reviews":50,
+    "price":1500,
+    "description":"This is iPhone, apple topnotch flagship device."
+ }
+
   constructor() { }
+
+  addToCart() {
+    console.log('Add to Cart called')
+  }
 
   ngOnInit(): void {
   }
